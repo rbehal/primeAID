8a0fd13cb68b6d22b3ac4d4ef19ebbefe5d106d8
kdassharma <kaustav.dassharma@mail.mcgill.ca>
2020-01-25 15:56:04 -0500

    Added bootstrap modal for symptoms

--- maps.js
+++ maps.js
@@ -4,10 +4,12 @@ var longitude = -73.567253;
 function initMap() {
     var myLatlng = new google.maps.LatLng(latitude,longitude);
     var mapOptions = {
-        zoom: 16,
+        zoom: 17.5,
         center: myLatlng,
         mapTypeId: 'satellite'
     };
     var map = new google.maps.Map(document.getElementById('map'),
     mapOptions);
+    map.setTilt(45);
+
 }
