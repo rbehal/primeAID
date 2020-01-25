15ecc4bec2891e43805938e88a0b0b97d5ad239b
kdassharma <kaustav.dassharma@mail.mcgill.ca>
2020-01-25 14:30:15 -0500

    Added bootstrap

--- maps.js
+++ maps.js
@@ -1,6 +1,8 @@
 var map;
+var latitude = 45.501690;
+var longitude = -73.567253;
 function initMap() {
-    var myLatlng = new google.maps.LatLng(45.501690,-73.567253);
+    var myLatlng = new google.maps.LatLng(latitude,longitude);
     var mapOptions = {
         zoom: 16,
         center: myLatlng,
