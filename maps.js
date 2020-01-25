986376ffa60400e18a4275cf176b5688cb6ee6af
kdassharma <kaustav.dassharma@mail.mcgill.ca>
2020-01-25 16:12:54 -0500

    Added geolocator

--- maps.js
+++ maps.js
@@ -1,15 +1,40 @@
-var map;
 var latitude = 45.501690;
 var longitude = -73.567253;
-function initMap() {
-    var myLatlng = new google.maps.LatLng(latitude,longitude);
-    var mapOptions = {
-        zoom: 17.5,
-        center: myLatlng,
-        mapTypeId: 'satellite'
-    };
-    var map = new google.maps.Map(document.getElementById('map'),
-    mapOptions);
-    map.setTilt(45);
 
-}
+var map, infoWindow;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: latitude, lng: longitude},
+        zoom: 18,
+        mapTypeId: 'satellite'
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Your current location');
+        infoWindow.open(map);
+        map.setCenter(pos);
+        }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+    }
