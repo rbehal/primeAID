efb752b1cba514916eb2fabb6638383826dbbbac
rbehal <rahulbehal01@hotmail.com>
2020-01-25 16:23:50 -0500

    Searching symptoms popup. Added checkboxes.

--- search_bar.js
+++ search_bar.js
@@ -7,10 +7,24 @@ function myFunction() {
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
         txtValue = a.textContent || a.innerText;
+        li[i].style.display = "block";
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
         }
+        if (filter == "") {
+            li[i].style.display = "none"; 
+        }
     }
+}
+
+function hideSympt() {
+    var ul, li;
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        li[i].style.display = "none";
+    }
+    console.log("Func. work")
 }
