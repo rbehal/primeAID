b54f4b3c52dc745b17d23b252fcbefc4752a5c66
atma99 <rahula830@gmail.com>
2020-01-25 17:50:51 -0500

    Update search.js

--- search.js
+++ search.js
@@ -6,6 +6,9 @@ function searchFunction() {
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
+        if (i == 0) {
+            console.log(typeof a);
+        }
         txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
