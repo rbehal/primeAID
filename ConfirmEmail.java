a00570055d083f60e59a7778b665da8137c30c40
alexander <alexander@MacBook-Pro-5.local>
2020-01-26 05:34:00 -0500

    updated UI

--- ConfirmEmail.java
+++ ConfirmEmail.java
@@ -1,7 +1,6 @@
 package primeAID;
 
 import javax.swing.*;
-import com.sun.jdi.InternalException;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -24,15 +23,22 @@ public class ConfirmEmail {
         JLabel titleLabel = new JLabel();
         titleLabel.setText("Confirm Email");
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        titleLabel.setBackground(Color.GREEN);
+        titleLabel.setBackground(Color.BLACK);
         titleLabel.setOpaque(true);
+        titleLabel.setForeground(Color.WHITE);
         titleLabel.setBounds(0, 20,600, 60);
         titleLabel.setFont(new Font("Courier", Font.BOLD,40));
+        
+        JLabel image = new JLabel();
+        image.setIcon(new ImageIcon("./src/primeAID/media/PLogo.png"));
+        image.setVisible(true);
+        image.setSize(300, 130);
+        image.setLocation(55, 215);
         
         JLabel emailLabel = new JLabel();
         emailLabel.setText("Email: ");
         emailLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-        emailLabel.setBackground(Color.LIGHT_GRAY);
+        emailLabel.setBackground(Color.WHITE);
         emailLabel.setOpaque(true);
         emailLabel.setBounds(0, 125, 240, 25);
         
@@ -44,19 +50,20 @@ public class ConfirmEmail {
         
         JButton loginButton = new JButton("Check!");
         loginButton.setBounds(300, 250, 130, 60);
-        loginButton.setBorder(BorderFactory.createLineBorder(Color.GREEN, 4));
+        loginButton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 4));
         loginButton.setOpaque(true);
         
         JLabel errorMessage = new JLabel();
         errorMessage.setText("");
-        errorMessage.setBounds(0, 330, 3000, 20);
+        errorMessage.setBounds(0, 330, 600, 20);
         errorMessage.setVisible(false);
         errorMessage.setBackground(Color.WHITE);
         errorMessage.setForeground(Color.RED);
         errorMessage.setOpaque(true);
+        errorMessage.setHorizontalAlignment(SwingConstants.CENTER);
 
         JPanel panel = new JPanel();
-        panel.setBackground(Color.DARK_GRAY);
+        panel.setBackground(Color.WHITE);
         panel.setSize(600, 390);
         panel.setLayout(null);
         panel.setLocation(0, 0);
@@ -67,7 +74,7 @@ public class ConfirmEmail {
         panel.add(adminPasswordLabel);
         panel.add(errorMessage);
         panel.add(loginButton);
-
+        panel.add(image);
         panel.setVisible(true);
      
         frame.add(panel);
